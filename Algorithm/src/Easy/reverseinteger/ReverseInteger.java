389b14fcbbfef3d9d08552d27cb28ecc9ec1c4de
chuksabadom <Donmeks85>
2021-07-28 12:49:27 +0200

    reverse-integer

--- Algorithm/src/Easy/reverseinteger/ReverseInteger.java
+++ Algorithm/src/Easy/reverseinteger/ReverseInteger.java
@@ -1,4 +1,6 @@
-class Solution {
+package Easy.reverseinteger;
+
+public class ReverseInteger {
     public int reverse(int x) {
         if (x >= Integer.MAX_VALUE || x <= Integer.MIN_VALUE) return 0;
         String reversed = new StringBuilder().append(Math.abs(x)).reverse().toString();
@@ -6,6 +8,6 @@ class Solution {
             return (x < 0) ? Integer.parseInt(reversed) * -1 : Integer.parseInt(reversed);
         } catch (NumberFormatException e) {
             return 0;
-        }        
+        }
     }
 }
